c618c25a34f8c8759b8f54d9d9e9b9c9e76c1ac7
Sept28 <septianfaiz28@gmail.com>
2021-11-20 21:58:24 +0700

    layouts

---
+++ resources/views/admin/includes/script.blade.php
@@ -0,0 +1,20 @@
+  <!-- plugins:js -->
+  <script src="{{ asset('template/vendors/js/vendor.bundle.base.js') }}"></script>
+  <!-- endinject -->
+  <!-- Plugin js for this page -->
+  <script src="{{ asset('template/vendors/chart.js/Chart.min.js') }}"></script>
+  <script src="{{ asset('template/vendors/bootstrap-datepicker/bootstrap-datepicker.min.js') }}"></script>
+  <script src="{{ asset('template/vendors/progressbar.js/progressbar.min.js') }}"></script>
+
+  <!-- End plugin js for this page -->
+  <!-- inject:js -->
+  <script src="{{ asset('template/js/off-canvas.js') }}"></script>
+  <script src="{{ asset('template/js/hoverable-collapse.js') }}"></script>
+  <script src="{{ asset('template/js/template.js') }}"></script>
+  <script src="{{ asset('template/js/settings.js') }}"></script>
+  <script src="{{ asset('template/js/todolist.js') }}"></script>
+  <!-- endinject -->
+  <!-- Custom js for this page-->
+  <script src="{{ asset('template/js/dashboard.js') }}"></script>
+  <script src="{{ asset('template/js/Chart.roundedBarCharts.js') }}"></script>
+  <!-- End custom js for this page-->
---
+++ resources/views/admin/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @include('admin.includes.style')
+</head>
+<body>
+  <div class="container-scroller"> 
+    @include('admin.includes.navbar')
+    <!-- partial -->
+    <div class="container-fluid page-body-wrapper">
+      @include('admin.includes.sidebar')
+      @include('admin.includes.right-sidebar')
+      @yield('pages.index')
+      <!-- main-panel ends -->
+    </div>
+    <!-- page-body-wrapper ends -->
+  </div>
+  <!-- container-scroller -->
+
+  @include('admin.includes.script')
+</body>
+
+</html>
