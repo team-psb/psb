69ba9e6354d386b584ec1a1a1dc46ec9f2cb5765
RamaDias123 <Nurdias45@gmail.com>
2021-11-21 08:59:00 +0700

    tes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,6 +20,7 @@
                 font-family: 'Nunito', sans-serif;
             }
         </style>
+        <script src="https://unpkg.com/vue@next"></script>
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
