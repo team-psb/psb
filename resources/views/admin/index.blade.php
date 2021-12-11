8fd5e7a0e069a8acbfce5791fcc733353ccbb534
muji101 <mujikuwat91@gmail.com>
2021-12-11 20:50:35 +0700

    fix bug

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -516,7 +516,7 @@
     <footer class="footer">
         <div class="d-sm-flex justify-content-center justify-content-sm-between">
         <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Owner <a href="https://www.pondokinformatika.com/" target="_blank">Pondok Informatika Al Madinah</a> from psb team.</span>
-        <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Copyright © 2022. All rights reserved.</span>
+        <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Copyright © {{ date('Y') }}. All rights reserved.</span>
         </div>
     </footer>
     <!-- partial -->
