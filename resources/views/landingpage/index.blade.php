49201f92a08a8f82fe4933f95c974c6c39bf4685
muji101 <mujikuwat91@gmail.com>
2022-03-12 10:15:59 +0700

    cek bug

--- resources/views/landingpage/index.blade.php
+++ resources/views/landingpage/index.blade.php
@@ -2535,8 +2535,8 @@
 
             $("#full_name").on('keyup', function(e) {
                 var val = $(this).val();
-            if (val.match(/[^a-zA-Z]/g)) {
-                $(this).val(val.replace(/[^a-zA-Z]/g, ''));
+            if (val.match(/[^a-zA-Z\s]/g)) {
+                $(this).val(val.replace(/[^a-zA-Z\s]/g, ''));
             }
             });
         </script>
