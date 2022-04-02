b0e8493944fe2fa8d942c976e1077eceed16b3f9
Sept <septianfaiz28@gmail.com>
2022-04-02 09:35:36 +0700

    dashboard user

--- resources/views/front/includes/script.blade.php
+++ resources/views/front/includes/script.blade.php
@@ -9,6 +9,10 @@
   <!-- JS Libraies -->
   <script src="{{ asset('stisla/node_modules/chocolat/dist/js/jquery.chocolat.min.js') }}"></script>
   <script src="{{ asset('stisla/node_modules/jquery-ui-dist/jquery-ui.min.js') }}"></script>
+  <script src="../node_modules/jquery-sparkline/jquery.sparkline.min.js"></script>
+  <script src="../node_modules/chart.js/dist/Chart.min.js"></script>
+  <script src="{{ asset('stisla/node_modules/owl.carousel/dist/owl.carousel.min.js') }}"></script>
+  <script src="../node_modules/summernote/dist/summernote-bs4.js"></script>
   <!-- Page Specific JS File -->
 
   <!-- Template JS File -->
--- resources/views/front/includes/style.blade.php
+++ resources/views/front/includes/style.blade.php
@@ -11,6 +11,10 @@
 <!-- CSS Libraries -->
 <link rel="stylesheet" href="{{ asset('stisla/node_modules/chocolat/dist/css/chocolat.css') }}">
 <link rel="stylesheet" href="{{ asset('stisla/node_modules/prismjs/themes/prism.css') }}">
+  <link rel="stylesheet" href="../node_modules/jqvmap/dist/jqvmap.min.css">
+  <link rel="stylesheet" href="../node_modules/summernote/dist/summernote-bs4.css">
+  <link rel="stylesheet" href="{{ asset('stisla/node_modules/owl.carousel/dist/assets/owl.carousel.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('stisla/node_modules/owl.carousel/dist/assets/owl.theme.default.min.css') }}">
 
 <!-- Template CSS -->
 <link rel="stylesheet" href="{{ asset('stisla/assets/css/style.css') }}">
