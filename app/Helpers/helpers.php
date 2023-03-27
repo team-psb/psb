c2d641e007382e90e37d42a6b048ad348e154203
muji101 <mujikuwat91@gmail.com>
2023-03-27 20:18:10 +0700

    test wa

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -9,6 +9,7 @@ function sendMessage($data)
     try {
         $appUrlFontee = "https://api.fonnte.com/send";
         $tokenFontee = 'azirSQttjrWI7QuIZA73';
+
         // $tokenFontee = '!eciHq9Szhs3863ra63z';
 
         $response = Http::withHeaders([
